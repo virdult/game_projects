4cf24a51bf95397160d77c90e56dbdc65d2953c7
virdult <virdult@gmail.com>
2026-02-21 17:48:09 +0300

    chore: repository cleaned, only source files kept

--- newGame/CMakeFiles/TerminalGame.dir/src/projectiles.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/TerminalGame.dir/src/projectiles.cpp.o: \
- /home/virdult/game/newGame/src/projectiles.cpp \
- /usr/include/stdc-predef.h \
- /home/virdult/game/newGame/include/projectiles.hpp
